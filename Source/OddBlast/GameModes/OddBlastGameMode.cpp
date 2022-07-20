263f7eb094808ad821ae19f1082ee56cbdca961d
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-07-20 12:18:14 +0100

    Fix Unfound Class at editor launch

--- Source/OddBlast/GameModes/OddBlastGameMode.cpp
+++ Source/OddBlast/GameModes/OddBlastGameMode.cpp
@@ -1,16 +1,10 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "OddBlastGameMode.h"
-#include "UObject/ConstructorHelpers.h"
-#include "../Characters/PlayerCharacter.h"
 
 AOddBlastGameMode::AOddBlastGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Core/Character/Player/BP_FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
 }
 
 void AOddBlastGameMode::EndGame(bool bIsPlayerWinner)
