f7ee3eef904e4771c1dd378d5c03dbfbf497e43d
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-07-21 12:40:11 +0100

    feat: create BlockProjectile class

---
+++ Source/OddBlast/Items/BlockProjectile.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../Items/BlockProjectile.h"
+
+void ABlockProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+}
